9fb3ca38a531ff72c92883861c5cd354180f9a24
gunnardraft <gunnardraft@live.com.mx>
2023-12-19 17:43:03 -0600

    add google map api

--- traxi-technical-test/src/app/page.tsx
+++ traxi-technical-test/src/app/page.tsx
@@ -1,4 +1,17 @@
+'use client'
+import { useCallback, useState } from 'react';
 import styles from './page.module.css'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
 
 // async function getData() {
 //   const res = await fetch('')
@@ -11,12 +24,24 @@ import styles from './page.module.css'
 // }
 
 
-export default async function Home() {
+export default function Home() {
   // const data = await getData()
-
+  const [map, setMap] = useState(null)
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_API_KEY || "AIzaSyCByFHPYqmrJIPJKDzyw2sEbYz4jZR2YVE"
+  })
+  
   return (
     <main className={styles.main}>
-      <div className={styles.description}> 
+      <div className={styles.description}>
+        {isLoaded ? <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+        </GoogleMap> : <></>}
+   
       </div>
     </main>
   )
