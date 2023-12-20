7d92d752a0380f8860d3a4e28e8102f327aa5407
gunnardraft <gunnardraft@live.com.mx>
2023-12-19 20:35:59 -0600

    validacion de filtro

--- traxi-technical-test/src/app/page.tsx
+++ traxi-technical-test/src/app/page.tsx
@@ -39,7 +39,7 @@ interface IVehiculo {
   YEAR: number;
   COLOR: string;
 }
- 
+
 export default function Home() {
   const [map, setMap] = useState(null)
   const { isLoaded } = useJsApiLoader({
@@ -54,12 +54,12 @@ export default function Home() {
     setFilter(event.target.value);
   };
 
-  const filteredVehicles = vehicleList.filter((vehicle) => { 
+  const filteredVehicles = vehicleList.filter((vehicle) => {
     return (vehicle.placa.toLowerCase().includes(filter.toLowerCase()))
   });
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
- 
+
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredVehicles.length) : 0;
 
@@ -76,7 +76,7 @@ export default function Home() {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-  const columns= [
+  const columns = [
     { field: 'id', headerName: 'Placa', width: 90 },
     {
       field: 'numero economico',
@@ -86,14 +86,14 @@ export default function Home() {
     },
     {
       field: 'vim',
-      headerName: 'VIM', 
+      headerName: 'VIM',
       width: 110,
       editable: false,
     },
     {
       field: 'asientos',
-      headerName: 'Full name',  
-      width: 160, 
+      headerName: 'Full name',
+      width: 160,
       editable: false,
     },
     {
@@ -153,92 +153,91 @@ export default function Home() {
           />
         </SearchVeicle>
         <VeicleList>
-          lista de veiculos
-
-          {/* {filteredVehicles.map((car) => <div id={car.placa}>Placa:{car.placa} Número Económico: {car["numero economico"]} VIM: {car.vim} Asientos: {car.asientos} Seguro: {car.seguro} Número de seguro: {car["segure numebr"]} Brand: {car.BRAND} Model: {car.MODEL} :Año: {car.YEAR} Color: {car.COLOR} </div>)} */}
-      
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="right">Placa</TableCell>
-                  <TableCell align="right">Número Económico</TableCell>
-                  <TableCell align="right">VIM</TableCell>
-                  <TableCell align="right">Asientos</TableCell>
-                  <TableCell align="right">Seguro</TableCell>
-                  <TableCell align="right">Número de seguro</TableCell>
-                  <TableCell align="right">Marca</TableCell>
-                  <TableCell align="right">Modelo</TableCell>
-                  <TableCell align="right">Año</TableCell>
-                  <TableCell align="right">Color</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {(rowsPerPage > 0
-                  ? filteredVehicles.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  : filteredVehicles
-                ).map((row) => (
-                  <TableRow key={row.placa}>
-                    <TableCell component="th" scope="row">
-                      {row.placa}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row['numero economico']}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row.vim}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row.asientos}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row.seguro}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row["segure numebr"]}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row.BRAND}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row.MODEL}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row.YEAR}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="right">
-                      {row.COLOR}
-                    </TableCell>
+          lista de veiculos 
+          {filteredVehicles.length > 0 ?
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">Placa</TableCell>
+                    <TableCell align="right">Número Económico</TableCell>
+                    <TableCell align="right">VIM</TableCell>
+                    <TableCell align="right">Asientos</TableCell>
+                    <TableCell align="right">Seguro</TableCell>
+                    <TableCell align="right">Número de seguro</TableCell>
+                    <TableCell align="right">Marca</TableCell>
+                    <TableCell align="right">Modelo</TableCell>
+                    <TableCell align="right">Año</TableCell>
+                    <TableCell align="right">Color</TableCell>
                   </TableRow>
-                ))}
-                {emptyRows > 0 && (
-                  <TableRow style={{ height: 53 * emptyRows }}>
-                    <TableCell colSpan={6} />
+                </TableHead>
+                <TableBody>
+                  {(rowsPerPage > 0
+                    ? filteredVehicles.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    : filteredVehicles
+                  ).map((row) => (
+                    <TableRow key={row.placa}>
+                      <TableCell component="th" scope="row">
+                        {row.placa}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row['numero economico']}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row.vim}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row.asientos}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row.seguro}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row["segure numebr"]}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row.BRAND}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row.MODEL}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row.YEAR}
+                      </TableCell>
+                      <TableCell style={{ width: 160 }} align="right">
+                        {row.COLOR}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                      colSpan={3}
+                      count={filteredVehicles.length}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      SelectProps={{
+                        inputProps: {
+                          'aria-label': 'rows per page',
+                        },
+                        native: true,
+                      }}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      ActionsComponent={TablePaginationActions}
+                    />
                   </TableRow>
-                )}
-              </TableBody>
-              <TableFooter>
-                <TableRow>
-                  <TablePagination
-                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                    colSpan={3}
-                    count={filteredVehicles.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    SelectProps={{
-                      inputProps: {
-                        'aria-label': 'rows per page',
-                      },
-                      native: true,
-                    }}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                    ActionsComponent={TablePaginationActions}
-                  />
-                </TableRow>
-              </TableFooter>
-            </Table>
-          </TableContainer>
+                </TableFooter>
+              </Table>
+            </TableContainer>
+            : <div>Sin Resultados</div>}
         </VeicleList>
       </MainContainer>
     </Main>
